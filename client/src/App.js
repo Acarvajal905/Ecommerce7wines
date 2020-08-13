98d5ec3270bf8768a5cff2e192996a3184e17e93
Guille <guillermocruz409@hotmail.com>
2020-08-13 16:29:44 -0300

    ruta de catalogo s14

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Producto from './components/producto.js'
-import { BrowserRouter } from 'react-router-dom'
+import Catalogo from './components/Catalogo.js'
+import { BrowserRouter,Switch, Route } from 'react-router-dom'
 import Searchbar from './components/searchbar.js'
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Switch>
+          <Route path="/products" component={Catalogo}/>
+      </Switch>
       <Searchbar />
       <Producto />
     </BrowserRouter>
