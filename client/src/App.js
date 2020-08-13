1a00b180bb1bc767da6c0dca27160232919b600f
RodriguezIbrahin <66222351+RodriguezIbrahin@users.noreply.github.com>
2020-08-13 16:05:37 -0300

    corregido catalogo, productcard y agregado filtrado

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import Producto from './components/producto.js'
+import Producto from './components/producto.js';
+import Catalogo from "./components/Catalogo.js"
+/* let tipo = "espumante";
 
+let articulo = [{name: "tinto",
+  categories: "vino",
+  price: "$10",
+  description: "nadan",
+  stock: 2,
+  image: "aca hay una foto",
+  content: "750cc",
+  percentage: "10%",
+  country: "argentina",
+  colour: "borboña",
+  quantity: "nose"},
+  {name: "blanco",
+  categories: "espumante",
+  price: "$10",
+  description: "nadan",
+  stock: 2,
+  image: "aca hay una foto",
+  content: "750cc",
+  percentage: "10%",
+  country: "argentina",
+  colour: "borboña",
+  quantity: "nose"}];
+ */
 function App() {
   return (
     <React.Fragment>
+     {/*  <Catalogo producto={articulo} categoria={tipo}/> */}
       <Producto />
     </React.Fragment>
 
