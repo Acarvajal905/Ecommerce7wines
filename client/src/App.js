0454e8c3ff3e9e936e917a8ee3deda160dd84cf5
RodriguezIbrahin <66222351+RodriguezIbrahin@users.noreply.github.com>
2020-08-13 18:32:39 -0300

    ahora

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import Searchbar from './components/searchbar.js'
 import Catalogo from "./components/Catalogo.js"
 
- let tipo = "tinto";
-
-let articulo = [{name: "tinto",
-  categories: "tinto",
-  price: "$10",
-  description: "nadan",
-  stock: 2,
-  image: "aca hay una foto",
-  content: "750cc",
-  percentage: "10%",
-  country: "argentina",
-  colour: "borboña",
-  quantity: "nose"}];
  
 function App() {
   return (
     <BrowserRouter>
     <Searchbar />
     <Producto />
-    <Catalogo producto={articulo} categoria={tipo}/>
+    <Catalogo />
   </BrowserRouter>
   );
 }
