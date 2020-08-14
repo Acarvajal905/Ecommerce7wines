1421bb6d92ad4467abbed3a1edd8f5694581d389
RodriguezIbrahin <66222351+RodriguezIbrahin@users.noreply.github.com>
2020-08-14 19:02:03 -0300

    rutas agregadas

--- client/src/components/CRUD/actualizarproducto.js
+++ client/src/components/CRUD/actualizarproducto.js
@@ -1,33 +1,63 @@
 import React from 'react';
 
-export default function Update(props) {
+export default function Update() {
 
-    <form onSubmit={props.create}>
-
-        <select
-            onChange={(x) =>
-                (x.target.value)}>
-            {
-                props.product.map((product) => {
-                    return <option key={product.name}> {product.name} </option>
-                })
-            }
-        </select>
-
-        <input type='text' value={product.name} name='name' />
-        <input type='text' value={product.description} name='description' />
-        <input type='text' value={product.price} name='price' />
-        <input type='text' value={product.stock} name='stock' />
-        <input type='text' value={product.categories} name='categories' />
-        <input type='text' value={product.name} name='name' />
-        <input type='text' value={product.quantity} name='quantity' />
-        <input type='text' value={product.content} name='content' />
-        <input type='text' value={product.percentage} name='percentage' />
-        <input type='text' value={product.country} name='country' />
-        <input type='text' value={product.colour} name='colour' />
-        <label>Sube aqui la imagen del producto</label>
-        <input type='file' id='imagen' name='imagen' />
-        <input type='submit' value='ACTUALIZAR' />
-
-    </form>
-}
+    const handleSumit = function(e) {
+        e.preventDefault();
+        /* Aca va la funcion para editar la bd, actualizando el producto */
+     };
+    
+     
+     return ( 
+        <form onSubmit={handleSumit}>
+          <div> <h1>Actualizar Producto</h1> </div>
+          <div>
+            <label>Nombre:</label>
+            <input type= 'text' placeholder = 'Nombre' name = 'name'></input>
+          </div>
+          <div>
+            <label>Descripcion:</label>
+            <input type= 'text' placeholder = 'Descripcion' name = 'description'></input>
+          </div>
+          <div>
+            <label>Precio:</label>
+            <input type= 'text' placeholder = 'Precio' name = 'price'></input>
+          </div>
+          <div>
+            <label>Stock:</label>
+            <input type= 'text' placeholder = 'Stock' name = 'stock'></input>
+          </div>
+          <div>
+            <label>Categories:</label>
+            <input type= 'text' placeholder = 'Categoria' name = 'categories'></input>
+          </div>
+          <div>
+            <label>Imagen:</label>
+            <input type= 'file' placeholder = 'montar imagen' name = 'image'></input> 
+          </div>
+          <div>
+            <label>Cantidad:</label>
+            <input type= 'text' placeholder = 'Cantindad' name = 'quantity'></input>
+          </div>
+          <div>
+            <label>Contenido:</label>
+            <input type= 'text' placeholder = 'Contenido' name = 'content'></input>
+          </div>
+          <div>
+            <label>Grado Alcoholico:</label>
+            <input type= 'text' placeholder = 'percentage' name = 'percentage'></input>
+          </div>
+          <div>
+            <label>Pais:</label>
+            <input type= 'text' placeholder = 'Pais' name = 'country'></input>
+          </div>
+          <div>
+            <label>Color:</label>
+            <input type= 'text' placeholder = 'Color' name = 'colour'></input>
+          </div>
+          <input type="submit" value="CREAR PRODUCTO" />
+    
+        </form>
+      )
+    };
+    
--- client/src/components/CRUD/crearcategoria.js
+++ client/src/components/CRUD/crearcategoria.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
-export default function NewCategory(props) {
+export default function NewCategory() {
+
+
+ const handleSumit = function(e) {
+      e.preventDefault();
+      /* Aca va la funcion para editar la bd, creando categoria */
+    };
+    
     return (
-        <form onSubmit={props.NewCategory}>
-            <input type='text' placeholder='Categoria nueva' name='NewCategory' />
-            <input type='text' placeholder='Descripcion de nueva categoria' name='CategoryDescription' />
+       <form onSubmit={handleSumit}>
+           <div> <h1>Crear Categoria</h1> </div>
+           <div>
+               <label>Nueva Categoria:</label>
+               <input type= 'text' placeholder = 'Categoria' name = 'NewCategory'></input>
+            </div>
+            <div>
+               <label>Descripcion:</label>
+               <input type= 'text' placeholder = 'Descripcion de nueva categoria' name = 'CategoryDescription'></input>
+            </div>
             <input type='submit' value='AGREGAR CATEGORIA' />
-        </form>
+       </form>
     )
 
 }
