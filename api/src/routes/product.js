c215f4645d53e61b427d3c8cfcc75acfe8ce8037
Guille <guillermocruz409@hotmail.com>
2020-08-13 22:57:09 -0300

    s23 y s21

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,29 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
+
 server.get('/', (req, res, next) => {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
+});
+server.get('/products/',(req,res,next)=>{
+	Product.findAll()
+		.then(products=>{
+			res.json(products);
+		}).catch(next)
+});
+server.get('/search',(req,res,next) => {
+	Product.findAll({
+		where:{
+			name:req.query.value,
+			description:req.query.value,
+		}
+	}).then(function(products){
+		res.render(products)
+	}).catch(next);
 });
 //S17 Crea ruta para agregar categorias de un producto.
 server.post('/:idProducto/category/:idCategory', (req, res) => {
