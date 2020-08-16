a817ca465166533ab1bc09717e7ea78961991128
RodriguezIbrahin <66222351+RodriguezIbrahin@users.noreply.github.com>
2020-08-15 23:01:31 -0300

    searchbar funcionanco pero con api

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
+import React from "react";
+import axios from "axios"
 
-export const SearchBar = ({ onSearch }) => {                  /* onSearch es la funcion para app donde buscaremos a la api */
-    const [name, setname] = useState("");
+export default function SearchBar () {          
+
+    const GetProducto = async (Prod) =>{  /* GetProducto se encarga de buscar en bd */
+        try{
+                                            /* Agustar la direccion de la bd */
+            const resProd = await axios (`https://jsonplaceholder.typicode.com/users/`) /* Aca trae todos los productos de la bd */
+            
+            let Buscado= resProd.data.filter(p => p.name == Prod)  /* Hace un filtrado para buscar en producto que se busca */
+            console.log(Buscado[0].id)
+
+            let Url = "http://localhost:3000/products/" + Buscado[0].id;  /* Genera el link segun el id */
+
+            console.log(Url)
+
+            window.location.href = Url /* Redirecciona a la url para que machee la app y renderice el producto */
+
+
+        } catch(error){
+            console.log(error);
+        }
+    }
+
+    const handleSumit = function(e) {
+        e.preventDefault();
+        /* Aca va la funcion buscar en la bd */
+           GetProducto(e.target.searchbar.value)
+      };
+
     return (
 
-        <form>
+        <form onSubmit={handleSumit}>
 
-            <input                                                 /* caja para escribir producto */
+            <input                                             /* caja para escribir producto */
                 type="text"
                 placeholder="Buscar Producto"
-                value={name}
-                onChange={e => setname(e.target.value)}
-            />
+                name ="searchbar"
+                ></input>
+                
 
-            <input type="submit" value="Buscar..." />               {/* Boton para buscar */}
+            <input type="submit" value="Buscar..." />              {/* Boton para buscar  */}
 
         </form>
     );
 }
-export default SearchBar;
