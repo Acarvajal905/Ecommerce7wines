d8f67881ac028721d9234c3328606b382fd4f488
Acarvajal905 <acarvajal905@gmail.com>
2020-08-18 19:59:18 -0300

    carrito

--- client/src/components/Catalogo.js
+++ client/src/components/Catalogo.js
@@ -1,49 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from "./productcard"; 
+import ProductCard from "./productcard";
 import "../Styles/Cards.css";
 import axios from "axios";
 
+
 // AJUSTAR PARAMETROS, ESTAN DEFINIDOS PARA LA API
 
-export default function Catalogo(){ 
+export default function Catalogo() {
 
     //define un estado al componente
 
- const [CatalogoInfo, setCatalogoInfo] = useState(null) 
+    const [CatalogoInfo, setCatalogoInfo] = useState(null)
 
-     // hace un get a la bd sobre los productos 
+    // hace un get a la bd sobre los productos 
 
- useEffect(() => {
-    axios.get(`http://localhost:3001/products`)
-      .then(function (response) {
-        setCatalogoInfo(response.data)
-        console.log(response.data)
-      })
-  }, [])  //ese array vacio, limita la actualizacion del componente al montarse
+    useEffect(() => {
+        axios.get(`http://localhost:3001/products`)
+            .then(function (response) {
+                setCatalogoInfo(response.data)
+                console.log(response.data)
+            })
+    }, [])  //ese array vacio, limita la actualizacion del componente al montarse
 
-   //retorno el estado y renderizo 
-   
-   return(                          //Definidos para la api                                                         
-    CatalogoInfo  && (             
-        <div class="box5">
-            { CatalogoInfo.map(v =>  
-             <ProductCard 
-                name = {v.name}
-                id={v.id}
-                description = {v.description}
-                price = {v.price}
-                stock = {v.stock}
-                image = {v.image}
-                quantity = {v.quantity} 
-                content = {v.content}
-                percentage = {v.percentage}
-                country = {v.country}
-                colour = {v.colour}
-                /> 
-                    )
+    //retorno el estado y renderizo 
+
+    return (                          //Definidos para la api                                                         
+        CatalogoInfo && (
+            <div class="box5">
+                {CatalogoInfo.map(v =>
+                    <ProductCard
+                        name={v.name}
+                        id={v.id}
+                        description={v.description}
+                        price={v.price}
+                        stock={v.stock}
+                        image={v.image}
+                        quantity={v.quantity}
+                        content={v.content}
+                        percentage={v.percentage}
+                        country={v.country}
+                        colour={v.colour}
+                    />
+                )
                 }
-        </div>
+            </div>
+        )
     )
-   ) 
 
 }
--- client/src/components/productcard.js
+++ client/src/components/productcard.js
@@ -5,20 +5,21 @@ import "../Styles/Cards.css"
 export default function ProductCard(props) { /* las props las recibe desde catalogo */
 
   return (
-      /* Podemos agregar mas destalles a la vista previa */
-    
-      <div class="box3">
-        {console.log(props)}
-        <Link to={`/products/${props.id}`} >   {/* Esta para que machee con la api */}
-          <h3>{props.name}</h3>
-        </Link>
-        <Link to={`/products/${props.id}`} >
+    /* Podemos agregar mas destalles a la vista previa */
+
+    <div class="box3">
+      {console.log(props)}
+      <Link to={`/products/${props.id}`} >   {/* Esta para que machee con la api */}
+        <h3>{props.name}</h3>
+      </Link>
+      <Link to={`/products/${props.id}`} >
         {/* <img>{props.image}</img> */}
         <div class="imagenF" >IMAGEN</div>
-        </Link>
-        <span>{props.price} $</span>
-        <button type="button" class="btn btn-info btn-sm">Agregar al carrito</button>
-      </div>
-    
+      </Link>
+      <span>{props.price} $</span>
+      <button type="button" class="btn btn-info btn-sm">Agregar al carrito</button>
+
+    </div>
+
   )
 };
