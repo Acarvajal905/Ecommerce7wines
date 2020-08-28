5abab381a23f90605ece1a99cd4d2745a49714d2
Miguel Angel Jaimes <66225919+MrFoxxz@users.noreply.github.com>
2020-08-28 16:45:54 -0500

    {fixes

--- client/src/components/Redux/Reducers/reducers.js
+++ client/src/components/Redux/Reducers/reducers.js
@@ -13,23 +13,22 @@ const initialState = {
 
   function rootReducer(state = initialState, action){
     if (action.type === GET_PRODUCTO) {
-        return {
-          ...state,
-          product: action.payload  // modifico product del store, agregando el producto seleccionado
-        }  
-
+      return {
+        ...state,
+        product: action.payload  // modifico product del store, agregando el producto seleccionado
+      }  
     }
-  
-  if (action.type === GET_ALL_PRODUCTS) {
-    return {
-      ...state,
-      allproducts: action.payload  //modifico allproducts del store, agregando todos los productos de la bd
+    if (action.type === GET_ALL_PRODUCTS) {
+      return {
+        ...state,
+        allproducts: action.payload  //modifico allproducts del store, agregando todos los productos de la bd
+      }
     }
-  }
-  if (action.type === GET_5_PRODUCTS) {
-    return {
-      ...state,
-      fiveproducts: action.payload  //modifico allproducts del store, agregando 5 productos de la bd
+    if (action.type === GET_5_PRODUCTS) {
+      return {
+        ...state,
+        fiveproducts: action.payload  //modifico allproducts del store, agregando 5 productos de la bd
+      }
     }
     if (action.type === GET_PRODUCT_CAT){
       return {
@@ -43,13 +42,13 @@ const initialState = {
         searchproduct: action.payload  // modifico searchproduct del store, agregando los producto que machean
       }
     }
-  }
-  if (action.type === ADD_TO_CARS) {
-    return {
-      ...state,
-      carrito: state.carrito.concat(action.payload) // modifico carrito del store, agregando los producto que agrege
+    
+    if (action.type === ADD_TO_CARS) {
+      return {
+        ...state,
+        carrito: state.carrito.concat(action.payload) // modifico carrito del store, agregando los producto que agrege
+      }
     }
-
     if (action.type === GET_ALL_CATEGORY){
       return {
         ...state,
