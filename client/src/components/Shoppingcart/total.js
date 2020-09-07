b4580d190a2dbdfba675972d3a158ee778ca2545
Miguel Angel Jaimes <66225919+MrFoxxz@users.noreply.github.com>
2020-09-07 04:11:34 -0500

    fdsf

--- client/src/components/Shoppingcart/total.js
+++ client/src/components/Shoppingcart/total.js
@@ -7,42 +7,26 @@ export default function Total(){
   let cantidad;
 
   if(!localStorage.getItem('cantidades')){
-
     cantidad = [];
 
   }else {
-
     cantidad = JSON.parse(localStorage.getItem('cantidades'));
-
   }
 
   let productosCarrito; 
 
   if(!localStorage.getItem('productos')){
-
     productosCarrito = [];
-
   }else {
-
     productosCarrito = JSON.parse(localStorage.getItem('productos'));
   }
 
- 
-
-  
-
   for(let i = 0; i < productosCarrito.length; i++){
-
     for(let j = 0; j< cantidad.length; j++){
-
       if(productosCarrito[i].id == cantidad[j][0]){
-
        productosCarrito[i].cantidad = cantidad[j][1]
-
       }
-
     }
-
   }
 
   let total = 0;
@@ -55,15 +39,14 @@ export default function Total(){
 
   return(
     <div class="totalbox">
-      
-      <h5 class="titulost">Productos:</h5>
-
+      <h5 class="titulost">LISTA DE COMPRA</h5>
       {productosCarrito.map(v=>
-
-      <p class="precioprod">{v.price * v.cantidad} $</p>)}
-
-      <h2 class="titulost">TOTAL:</h2>
-
+      <div>
+        <p class="nameprod">{v.name}</p>
+        <p class="precioprod">{v.price} x {v.cantidad} = {v.price * v.cantidad} $</p>
+      </div>
+      )}
+      <h2 class="titulost">TOTAL</h2>
       <p class="precioprod"> {total}$</p>
       
       <form  onSubmit={Comprar} >
